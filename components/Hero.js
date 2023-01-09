fa0a54050f3b6ec9d9dae377702c099d0092badc
m3taTron <rootUS3rr@pm.me>
2023-01-09 15:47:04 +0400

    productionv1

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="mt-8">
-      <img src="/assets/home_image.png" alt="hero cover" />
+      <img src="/assets/new_intro.png" alt="hero cover" />
     </div>
   );
 }
